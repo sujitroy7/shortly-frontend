620293866646c81a06cafd52dcad0413d0dad250
Sujit Roy <sujitroy78148@gmail.com>
2025-12-10 20:02:13 +0530

    add a section called RecentlLinks

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import { QuickCreate } from "@/components/dashboard/QuickCreate"
+import { RecentLinks } from "@/components/dashboard/RecentLinks"
 
 export default function DashboardPage() {
   const userFirstName = "Sujit" 
@@ -6,7 +7,9 @@ export default function DashboardPage() {
   return (
     <div className="container">
       <h1 className="mb-5 text-3xl font-bold tracking-tight">Welcome back {userFirstName}!</h1>
-        <QuickCreate />
+      <QuickCreate />
+      <RecentLinks />
     </div>
   )
 }
+
